3cca13bdcfd9f817a165dfef2d1160e711c7b110
AnandaGTPS <108647595+AnandaGTPS@users.noreply.github.com>
2022-07-30 19:30:50 +0700

    Add files via upload

---
+++ loli.js
@@ -0,0 +1,226 @@
+[{
+    "creator": "Ananda",
+    "number": "1",
+    " loli": "https://telegra.ph/file/f5e128eeb464d5285843b.png"
+},
+{
+    "creator": "Ananda",
+    "number": "2",
+    "loli": "https://telegra.ph/file/031653e5d5ac655450f14.png"
+},
+{
+    "creator": "Ananda",
+    "number": "3",
+    "loli": "https://telegra.ph/file/97373217a247b865a3e18.png"
+},
+{
+    "creator": "Ananda",
+    "number": "4",
+    "loli": "https://telegra.ph/file/6c95df3e3d679b455bfd3.png"
+},
+{
+    "creator": "Ananda",
+    "number": "5",
+    "loli": "https://telegra.ph/file/6011bf48a7166c6619d86.png"
+},
+{
+    "creator": "Ananda",
+    "number": "6",
+    "loli": "https://telegra.ph/file/41ece74158fecc97a2d88.png"
+},
+{
+    "creator": "Ananda",
+    "number": "7",
+    "loli": "https://telegra.ph/file/f67aaaa96f4f22d5238cd.png"
+},
+{
+    "creator": "Ananda",
+    "number": "8",
+    "loli": "https://telegra.ph/file/f72156aa0fa0f35295d91.png"
+},
+{
+    "creator": "Ananda",
+    "number": "9",
+    "loli": "https://telegra.ph/file/2d4a9d37e3b1b6f3f88e6.png"
+},
+{
+    "creator": "Ananda",
+    "number": "10",
+    "loli": "https://telegra.ph/file/67da2d7d5c6540f138709.png"
+},
+{
+    "creator": "Ananda",
+    "number": "11",
+    "loli": "https://telegra.ph/file/fefccb3a0f26384eab78a.png"
+},
+{
+    "creator": "Ananda",
+    "number": "12",
+    "loli": "https://telegra.ph/file/2be25679008dc32ef0d45.png"
+},
+{
+    "creator": "Ananda",
+    "number": "13",
+    "loli": "https://telegra.ph/file/9e9589f3cb45bc531ee88.png"
+},
+{
+    "creator": "Ananda",
+    "number": "14",
+    "loli": "https://telegra.ph/file/2fd91f4b0cac77771e562.png"
+},
+{
+    "creator": "Ananda",
+    "number": "15",
+    "loli": "https://telegra.ph/file/4992ed8d8cf6661c9d16c.png"
+},
+{
+    "creator": "Ananda",
+    "number": "16",
+    "loli": "https://telegra.ph/file/b395e89a3da342e92562d.png"
+},
+{
+    "creator": "Ananda",
+    "number": "17",
+    "loli": "https://telegra.ph/file/f2fc616069324de90863f.png"
+},
+{
+    "creator": "Ananda",
+    "number": "18",
+    "loli": "https://telegra.ph/file/2b61c27e4abde7e559a46.png"
+},
+{
+    "creator": "Ananda",
+    "number": "19",
+    "loli": "https://telegra.ph/file/552cf48ee292d8d1185dd.jpg"
+},
+{
+    "creator": "Ananda",
+    "number": "20",
+    "loli": "https://telegra.ph/file/fe82815ef3d69d42af861.jpg"
+},
+{
+    "creator": "Ananda",
+    "number": "21",
+    "loli": "https://telegra.ph/file/27d270260e1b8f5091b82.jpg"
+},
+{
+    "creator": "Ananda",
+    "number": "22",
+    "loli": "https://telegra.ph/file/35593ec6b74350f0f7151.gif"
+},
+{
+    "creator": "Ananda",
+    "number": "23",
+    "loli": "https://telegra.ph/file/1d94a9447bd1017983357.gif"
+},
+ {
+    "creator": "Ananda",
+    "number": "24",
+    "loli": "https://telegra.ph/file/ce3a78863c3a5f46d1828.png"
+},
+{
+    "creator": "Ananda",
+    "number": "25",
+    "loli": "https://telegra.ph/file/3558890399cd7a2f1b5b0.jpg"
+},
+{
+    "creator": "Ananda",
+    "number": "26",
+    "loli": "https://telegra.ph/file/0b795fd076155b7d63e8a.png"
+},
+{
+    "creator": "Ananda",
+    "number": "27",
+    "loli": "https://telegra.ph/file/8d8fb91dc14b2862ee562.jpg"
+},
+{
+    "creator": "Ananda",
+    "number": "28",
+    "loli": "https://telegra.ph/file/8d8fb91dc14b2862ee562.jpg"
+},
+{
+    "creator": "Ananda",
+    "number": "https://telegra.ph/file/16b5512855c36b6710692.jpg",
+    "loli": ""
+},
+{
+    "creator": "Ananda",
+    "number": "30",
+    "loli": "https://telegra.ph/file/d4c73a15c52d34e14ae9e.jpg"
+},
+{
+    "creator": "Ananda",
+    "number": "31",
+    "loli": "https://telegra.ph/file/cb251358753123e4ecf49.png"
+},
+{
+    "creator": "Ananda",
+    "number": "32",
+    "loli": "https://telegra.ph/file/954856ff7ed9a3e3184ba.png"
+},
+ {
+    "creator": "Ananda",
+    "number": "33",
+    "loli": "https://telegra.ph/file/a998cb23a506bbc33122b.png"
+},
+{
+    "creator": "Ananda",
+    "number": "34",
+    "loli": "https://telegra.ph/file/9305b10eea7830f813ca5.png"
+},
+{
+    "creator": "Ananda",
+    "number": "35",
+    "loli": "https://telegra.ph/file/97483c064df4e4cce9c9b.png"
+},
+{
+    "creator": "Ananda",
+    "number": "36",
+    "loli": "https://telegra.ph/file/cacde31f16aaf83c39eed.png"
+},
+{
+    "creator": "Ananda",
+    "number": "37",
+    "loli": "https://telegra.ph/file/8951d50e9e702f430d4d4.png"
+},
+{
+    "creator": "Ananda",
+    "number": "38",
+    "loli": "https://telegra.ph/file/de70daab1dbcee65ea96f.png"
+},
+{
+    "creator": "Ananda",
+    "number": "39",
+    "loli": "https://telegra.ph/file/1b6124721070b3520ffa1.png"
+},
+{
+    "creator": "Ananda",
+    "number": "40",
+    "loli": "https://telegra.ph/file/abd8fc036cd23372a49a4.png"
+},
+{
+    "creator": "Ananda",
+    "number": "41",
+    "loli": "https://telegra.ph/file/96437ce64acbd4c7f411e.png"
+},
+{
+    "creator": "Ananda",
+    "number": "42",
+    "loli": "https://telegra.ph/file/55c42f9d4a9c5d4ee026f.png"
+},
+{
+    "creator": "Ananda",
+    "number": "43",
+    "loli": "https://telegra.ph/file/85974b0ca94626e7bba1f.png"
+},
+{
+    "creator": "Ananda",
+    "number": "44",
+    "loli": "https://telegra.ph/file/421ab9bdbfe5ed9b5bd43.png"
+},
+{
+    "creator": "Ananda",
+    "number": "45",
+    "loli": "https://telegra.ph/file/a7fef56df334f2b4bc223.jpg"
+}]
+
