bb10b3d1b17b1acd19bd1f98f5f2d0de85236f05
AnandaGTPS <108647595+AnandaGTPS@users.noreply.github.com>
2022-07-30 19:29:43 +0700

    Add files via upload

---
+++ meme.js
@@ -0,0 +1,100 @@
+[{
+    "creator": "Ananda",
+    "number": "1",
+    "meme": "https://telegra.ph/file/23b03054360b756931e2d.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "2",
+    "meme": "https://telegra.ph/file/833c6d8c0374c003fd5d1.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "3",
+    "meme": "https://telegra.ph/file/d6387365c4bd73cb0969f.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "4",
+    "meme": "https://telegra.ph/file/9aad2e75f0927c1710386.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "5",
+    "meme": "https://telegra.ph/file/605ee0ff09942ab78febd.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "6",
+    "meme": "https://telegra.ph/file/29e8a6e21944d7fa92fe5.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "7",
+    "meme": "https://telegra.ph/file/fe78bb73a237add693fd2.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "8",
+    "meme": "https://telegra.ph/file/81d8d1684231f4ec76e61.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "9",
+    "meme": "https://telegra.ph/file/b005bb153ae80534e0af8.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "10",
+    "meme": "https://telegra.ph/file/60fb1068c652e9b377d3f.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "11",
+    "meme": "https://telegra.ph/file/48d243b4c66e4f029bfff.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "12",
+    "meme": "https://telegra.ph/file/5b2a3d631de21a65acc2c.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "13",
+    "meme": "https://telegra.ph/file/049e477ec062a38566498.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "14",
+    "meme": "https://telegra.ph/file/bb8b824bae552bff24900.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "15",
+    "meme": "https://telegra.ph/file/a999c401e7ce887e5a445.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "16",
+    "meme": "https://telegra.ph/file/47aa0eccb4a2042300953.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "17",
+    "meme": "https://telegra.ph/file/47ea43fd58ea905e61cdf.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "18",
+    "meme": "https://telegra.ph/file/0a13d06137f00a6ff3f84.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "19",
+    "meme": "https://telegra.ph/file/89e149d83861d00a4857a.mp4"
+},
+{
+    "creator": "Ananda",
+    "number": "20",
+    "meme": "https://telegra.ph/file/9241d59a31ac24670783a.mp4"
+}]
